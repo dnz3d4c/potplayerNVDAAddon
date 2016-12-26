4e3c76fa2c2af7b19bcac72c25044b9534198e18
dnz3d4c <hi@aheu.org>
2016-12-26 16:54:12 +0900

    Add pot player mini support

---
+++ appModules/potplayermini.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# appModule for Daum potplayer (automatic reading subtitle and play information)
+# This file part of Daum potplayer AppModule
+# Copyright <2016> aheu <advck1123 at GMail Dot com>, license details <license.txt>
+from potplayer import AppModule
